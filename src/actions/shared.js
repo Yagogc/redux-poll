a9d846fc94c40ca0806de91d0bbe40cc2e713dab
Yago Gonzalez <jagogc@gmail.com>
2018-08-06 12:39:18 +0100

    Add loading bar

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,15 +2,18 @@ import { getInitialData } from "./../utils/api";
 import { receivePolls } from "./polls";
 import { receiveUsers } from "./users";
 import { setAuthedUser } from "./authedUser";
+import { showLoading, hideLoading } from "react-redux-loading";
 
 const AUTHED_ID = "tylermcginnis";
 
 export const handleInitialData = () => {
   return dispatch => {
+    dispatch(showLoading());
     return getInitialData().then(({ users, polls }) => {
       dispatch(receiveUsers(users));
       dispatch(receivePolls(polls));
       dispatch(setAuthedUser(AUTHED_ID));
+      dispatch(hideLoading());
     });
   };
 };
