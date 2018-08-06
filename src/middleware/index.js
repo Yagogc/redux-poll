821e6bf6298a268e00a9d88fd29e5f16e1b8e397
Yago Gonzalez <jagogc@gmail.com>
2018-08-06 16:47:59 +0100

    Add ReduxDevTools

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,6 @@
 import { applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 
-export default applyMiddleware(thunk, logger);
+export default composeWithDevTools(applyMiddleware(thunk, logger));
